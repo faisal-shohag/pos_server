d99016dd187ad1e6a831e0a5600dd336c1656349
faisal-shohag <faisalshohagprog@gmail.com>
2024-04-19 19:54:02 +0600

    added vercel

--- Controller/AuthController.js
+++ Controller/AuthController.js
@@ -12,7 +12,7 @@ const admin_login = async (req, res) => {
     try {
         const admin = await find_admin(email, password)
         const token = createToken(admin.id)
-        res.cookie('jwt_admin', token, { httpOnly: true, maxAge: maxAge * 1000, domain:'vercel.app'})
+        res.cookie('jwt_admin', token, { httpOnly: true, maxAge: maxAge * 1000})
         res.status(200).json({...admin, token})
     } catch (error) {
         const errors = handleError(error)
@@ -23,7 +23,7 @@ const admin_login = async (req, res) => {
 //admin logout
 const admin_logout = async(req, res) => {
     try{
-        res.cookie('jwt_admin', '', {httpOnly: true, maxAge: 1, domain:'vercel.app'})
+        res.cookie('jwt_admin', '', {httpOnly: true, maxAge: 1})
         res.status(200).json({"message": "logged out!"})
     } catch(error) {
         const errors = handleError(error)
@@ -37,7 +37,7 @@ const staff_login = async (req, res) => {
     try {
         const staff = await find_staff(email, password)
         const token = createToken(staff.email)
-        res.cookie('jwt_staff', token, { httpOnly: true, maxAge: maxAge * 1000, domain:'vercel.app'})
+        res.cookie('jwt_staff', token, { httpOnly: true, maxAge: maxAge * 1000})
         res.status(200).json(staff)
     } catch (error) {
         const errors = handleError(error)
@@ -49,7 +49,7 @@ const staff_login = async (req, res) => {
 //staff logout
 const staff_logout = async(req, res) => {
     try{
-        res.cookie('jwt_staff', '', {maxAge: 1, domain:'vercel.app'})
+        res.cookie('jwt_staff', '', {maxAge: 1})
         res.status(200).json({"message": "logged out!"})
     } catch(error) {
         const errors = handleError(error)
